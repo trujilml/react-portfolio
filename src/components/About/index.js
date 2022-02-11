40ea56b7319c830ed4b2210d6c094d89570d3fb7
trujilml <mickeytrj@gmail.com>
2022-02-11 13:47:38 -0600

    completed about section - move on to other components to finish and update with relevant css design

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import jpg from './trujillom.jpg';
 
 function About() {
     return (
-        <section>
-            <h1 id="about">About Mekayla Trujillo</h1>
+        <section className="container">
+            <img src={jpg} id="avatar" class="my-2"  alt="Mekayla Trujillo" />
+            <h2 id="about">About Mekayla Trujillo</h2>
             <p>I am a Full Stack Web Developer who loves technology and has a strong interest in coding new applications for the online world.</p>
             <p>My technical skills involve the use of JavaScript, the MERN Stack (MongoDB, Express, React, Node.js), MySQL, Mongoose, HTML, and CSS.</p>
             <p>When not coding, I find myself either baking, working out, chatting with friends, or playing with my pets.</p>
